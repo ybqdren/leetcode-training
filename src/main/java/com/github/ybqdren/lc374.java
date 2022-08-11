86d09f971d3c095708444ca4a6c5068ca4787b13
ybqdren <withzhaowen@126.com>
2022-08-11 22:26:38 +0800

    update lc347

---
+++ src/main/java/com/github/ybqdren/lc374.java
@@ -0,0 +1,28 @@
+package com.github.ybqdren;
+
+/**
+ * created by ybqdren
+ */
+public class lc374 {
+    public int guessNumber(int n) {
+        int left = 1 , right = n;
+        while(left <= right){
+            int mid = (right - left) /2 + left;
+            int guessResult = guess(mid);
+            if(guessResult == -1){
+                right = mid -1;
+            }else if(guessResult == 1){
+                left = mid + 1;
+            }else if(guessResult == 0){
+                return mid;
+            }
+        }
+
+        return 0;
+    }
+
+    // mooc interface
+    int guess(int num){
+        return 0;
+    }
+}
