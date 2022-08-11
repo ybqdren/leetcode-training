9fdfcc7ca1e710ea590cabee7d5a9e4726b51ab5
ybqdren <withzhaowen@126.com>
2022-08-11 22:41:20 +0800

    update lc35k

---
+++ src/main/java/com/github/ybqdren/lc35.java
@@ -0,0 +1,25 @@
+package com.github.ybqdren;
+
+/**
+ * 不用去单独考虑=mid的情况
+ *
+ * created by ybqdren
+ */
+public class lc35 {
+    public int searchInsert(int[] nums, int target) {
+        int length = nums.length;
+        int left =0 , right= length -1;
+        int result = length;
+        while (left <= right){
+            int mid = (right-left)/2 + left;
+            if(target <= nums[mid]){
+                result = mid;
+                right = mid - 1;
+            }else if(target > nums[mid]){
+                left = mid +1;
+            }
+        }
+
+        return result;
+    }
+}
