20b63875b429d21ab434b6079eabdab28a2587a8
ybqdren <withzhaowen@126.com>
2022-08-13 20:10:51 +0800

    update lc744

---
+++ src/main/java/com/github/ybqdren/lc744.java
@@ -0,0 +1,23 @@
+package com.github.ybqdren;
+
+/**
+ * created by ybqdren
+ */
+public class lc744 {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int low = 0,high = letters.length-1;
+        int ans = low;
+        while(low <= high){
+            int mid = (high - low) /2 + low;
+            if(letters[mid] > target){
+                ans = mid;
+                high = mid - 1;;
+            }else{
+                low = mid + 1;
+            }
+        }
+
+
+        return letters[ans];
+    }
+}
